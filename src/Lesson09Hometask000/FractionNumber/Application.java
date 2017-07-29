348de5ba9fb5ae3f5a6ae1109ab9fcb1e69a820b
vitaliy-tsvetkov <mr.megaboss@gmail.com>
2017-07-29 23:26:32 +0300

    uploading project

---
+++ src/Lesson09Hometask000/FractionNumber/Application.java
@@ -0,0 +1,28 @@
+package Lesson09Hometask000.FractionNumber;
+
+/**
+ * Created by Vitaliy Tsvetkov on 22.07.2017.
+ */
+
+//реализовать equals в FractionNumber. Сравнение FractionNumber - надо сделать
+
+public class Application {
+    public static void main(String[] args) {
+        FractionNumbers num1 = new FractionNumbers(3, 4);
+        FractionNumbers num2 = new FractionNumbers(8, 11);
+
+        FractionNumbers sumNumber = num1.plus(num2);
+        FractionNumbers minusNumber = num1.minus(num2);
+        FractionNumbers multiplyNumber = num1.multiply(num2);
+        FractionNumbers divideNumber = num1.divide(num2);
+
+        System.out.println("Первая дробь: " + num1);
+        System.out.println("Вторая дробь: " + num2);
+        System.out.println("\nСумма дробей: \t\t" + num1 + " + " + num2 + " = " + sumNumber);
+        System.out.println("Разность дробей: \t" + num1 + " - " + num2 + " = " + minusNumber);
+        System.out.println("Умножение дробей: \t" + num1 + " * " + num2 + " = " + multiplyNumber);
+        System.out.println("Деление дробей: \t" + num1 + " / " + num2 + " = " + divideNumber);
+
+        System.out.println(num1.equals(num2));
+    }
+}
